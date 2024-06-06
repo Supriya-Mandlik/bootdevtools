81f37744180d3072d705e05deb3db8abb12c6382
Supriya-Mandlik <158532483+Supriya-Mandlik@users.noreply.github.com>
2024-06-06 13:18:26 +0530

    commit

---
+++ bootdevtools/src/main/java/com/example/bootdevtools/TestController.java
@@ -0,0 +1,20 @@
+package com.example.bootdevtools;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test() {
+		int a=67;
+		int b=89;
+		int c=20;
+		int d=100;
+		return "this is just testing sum of a and b is "+(a+b+c+d);
+	}
+
+}
